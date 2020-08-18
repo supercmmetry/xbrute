9548cf6fb2845a0982f87875973411a7f4993eaf
supercmmetry <vishaals2000@gmail.com>
2020-08-18 18:40:17 +0530

    service: implement decryption
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- pkg/task/entity.go
+++ pkg/task/entity.go
@@ -1,13 +1,27 @@
 package task
 
 type Payload struct {
-	Start []byte
-	Count uint64
+	Start    []byte `json:"start"`
+	Count    uint64 `json:"count"`
+	Alphabet []byte `json:"alphabet"`
 }
 
+type Algorithm uint64
+
+const (
+	AES Algorithm = iota
+	RSA
+)
+
 type Task struct {
-	Id          uint64
-	PayloadData Payload
-	Target      []byte
-	PartialData []byte
+	Id            uint64    `json:"id"`
+	PayloadData   Payload   `json:"payload"`
+	Target        []byte    `json:"target"`
+	PartialData   []byte    `json:"partial_data"`
+	AlgorithmUsed Algorithm `json:"algorithm"`
+}
+
+type Result struct {
+	Id     uint64 `json:"id"`
+	Output []byte `json:"output"`
 }
--- pkg/task/service.go
+++ pkg/task/service.go
@@ -1,11 +1,67 @@
 package task
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
 type Service struct {
-	tasks []Task
+	tasks map[uint64]Task
+}
+
+func (s *Service) AddTask(task Task) {
+	s.tasks[task.Id] = task
+}
+
+func (s *Service) Decrypt(task Task, key []byte) *Result {
+	decryptedData := make([]byte, 0)
+
+	if task.AlgorithmUsed == AES {
+		c, err := aes.NewCipher(key)
+		if err != nil {
+			return nil
+		}
+
+		gcm, err := cipher.NewGCM(c)
+		if err != nil {
+			return nil
+		}
+
+		nonceSize := gcm.NonceSize()
+		if len(task.Target) < nonceSize {
+			return nil
+		}
+
+		nonce, ciphertext := task.Target[:nonceSize], task.Target[nonceSize:]
+		decryptedData, err = gcm.Open(nil, nonce, ciphertext, nil)
+
+		if err != nil {
+			return nil
+		}
+	}
+
+	j := 0
+	for i := 0; i < len(decryptedData); i++ {
+		cbyte := decryptedData[i]
+
+		if task.PartialData[j] == cbyte {
+			j++
+			if j == len(task.PartialData) {
+				return &Result {
+					Id: task.Id,
+					Output: key,
+				}
+			}
+		} else {
+			j = 0
+		}
+	}
+
+	return nil
 }
 
 func NewTaskService() Service {
 	return Service{
-		tasks: make([]Task, 0),
+		tasks: make(map[uint64]Task),
 	}
 }
